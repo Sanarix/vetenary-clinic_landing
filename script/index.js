cc0fe3b9b6bbbed41a99648b12a59fd6ea8100ec
Leonid Korsakov <KorsakovLA@mail.ru>
2022-06-16 12:32:12 +0300

    added index.js, added footer to index.html, added favicon

---
+++ script/index.js
@@ -0,0 +1,17 @@
+'use strict'
+const inputContainer = document.querySelector('.input-container');
+const emailInput = document.querySelector('.email-input');
+console.log(inputContainer);
+
+
+function borderHighlight () {
+	emailInput.onfocus = function() {
+		inputContainer.style.border = "2px solid grey";
+	}
+
+	emailInput.onblur = function() {
+		inputContainer.style.border = "none";
+	}
+}
+
+borderHighlight();
